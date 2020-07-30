e0eb084096609b03363b1b4b646f74da15840300
Frank Pintor <walter.pintor@gmx.com>
2020-07-30 15:08:32 +0800

    Added executable and dependencies
    Located in the dist folder.
    Other dependencies need to be installed and have been added inside folders.

---
+++ pyinstaller-develop/pyinstaller-develop/PyInstaller/utils/_gitrevision.py
@@ -0,0 +1,16 @@
+#
+# The content of this file will be filled in with meaningful data
+# when creating an archive using `git archive` or by downloading an
+# archive from github, e.g. from github.com/.../archive/develop.zip
+#
+rev = "2833160f3f"     # abbreviated commit hash
+commit = "2833160f3fa84ce02cb624547a8bcb22267c99f7"  # commit hash
+date = "2020-07-28 21:10:46 +1000"   # commit date
+author = "pyup.io bot <github-bot@pyup.io>"
+ref_names = "HEAD -> develop"  # incl. current branch
+commit_message = """Tests: Requirements: Scheduled weekly dependency update for week 30 (#5028)
+
+* Update simplejson from 3.17.0 to 3.17.2
+* Update numpy from 1.19.0 to 1.19.1
+* Update scipy from 1.5.0 to 1.5.2
+* Stop PyUp from pinning the dependencies in our requirements.txt file"""
